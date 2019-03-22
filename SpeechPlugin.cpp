3d870ecd240eea9ca322a380929cc4f501efe1b7
Vince <vin@vinsworld.com>
2019-03-22 12:53:41 -0400

    Updated project files, updated change log, updated RC for version after 64- and 32-bit tests.

--- SpeechPlugin.cpp
+++ SpeechPlugin.cpp
@@ -57,26 +57,19 @@ BOOL APIENTRY DllMain( HANDLE /*hModule*/,
 	{
 		case DLL_PROCESS_ATTACH:
 		{
+			lstrcpy(funcItem[0]._itemName, TEXT("Speak Selection"));
+			lstrcpy(funcItem[1]._itemName, TEXT("Speak Document"));
+			lstrcpy(funcItem[2]._itemName, TEXT("-SEPARATOR-"));
+			lstrcpy(funcItem[3]._itemName, TEXT("Stop Speech"));
+			lstrcpy(funcItem[4]._itemName, TEXT("Pause Speech"));
+			lstrcpy(funcItem[5]._itemName, TEXT("Resume Speech"));
+
 			funcItem[0]._pFunc = SpeakSelection;
 			funcItem[1]._pFunc = SpeakDocument;
-			funcItem[2]._pFunc = StopSpeech;
+			funcItem[2]._pFunc = NULL;
 			funcItem[3]._pFunc = StopSpeech;
 			funcItem[4]._pFunc = PauseSpeech;
 			funcItem[5]._pFunc = ResumeSpeech;
-
-			lstrcpy(funcItem[0]._itemName, __TEXT("Speak Selection"));
-			lstrcpy(funcItem[1]._itemName, __TEXT("Speak Document"));
-			lstrcpy(funcItem[2]._itemName, __TEXT("Stop Speech"));
-			lstrcpy(funcItem[3]._itemName, __TEXT("-"));
-			lstrcpy(funcItem[4]._itemName, __TEXT("Pause Speech"));
-			lstrcpy(funcItem[5]._itemName, __TEXT("Resume Speech"));
-
-			funcItem[0]._init2Check = false;
-			funcItem[1]._init2Check = false;
-			funcItem[2]._init2Check = false;
-			funcItem[3]._init2Check = false;
-			funcItem[4]._init2Check = false;
-			funcItem[5]._init2Check = false;
 
 			funcItem[0]._pShKey = NULL;
 			funcItem[1]._pShKey = NULL;
@@ -84,6 +77,13 @@ BOOL APIENTRY DllMain( HANDLE /*hModule*/,
 			funcItem[3]._pShKey = NULL;
 			funcItem[4]._pShKey = NULL;
 			funcItem[5]._pShKey = NULL;
+
+			funcItem[0]._init2Check = false;
+			funcItem[1]._init2Check = false;
+			funcItem[2]._init2Check = false;
+			funcItem[3]._init2Check = false;
+			funcItem[4]._init2Check = false;
+			funcItem[5]._init2Check = false;
 
 			gCanSpeek = SUCCEEDED(CoInitializeEx(NULL, COINIT_APARTMENTTHREADED));
 		}
